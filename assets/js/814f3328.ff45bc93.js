215b278abf679cdc2ac6aa1cb767893988bc528d
gulje <gyulje@proton.me>
2023-07-23 23:04:16 +0000

    Deploying to gh-pages from @ violet-eco/violet-eco.github.io@8a6905c3c74954e323711e6f8654ffa60aeb9b50 🚀

--- assets/js/814f3328.ff45bc93.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkviolet_eco_github_io=self.webpackChunkviolet_eco_github_io||[]).push([[2535],{5641:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"}]}')}}]);
